7e3ed2165efbdd1c98dea529c00df10cd3c81019
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-06-13 22:52:43 +0400

    Implement optimal K selection

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -7,18 +7,25 @@ K_Means = __import__('2-k_means').K_Means
 
 
 def optimal_k(X, max_clusters, random_state):
-    """Evaluate K-Means inertia and silhouette scores for different k values."""
+    """Evaluate inertia and silhouette scores."""
     ks = []
     inertia_values = []
     silhouette_values = []
 
     for k in range(2, max_clusters + 1):
-        model = K_Means(X, n_clusters=k, random_state=random_state)
+        model = K_Means(
+            X,
+            n_clusters=k,
+            random_state=random_state
+        )
 
         ks.append(k)
         inertia_values.append(model.inertia_)
 
-        score = metrics.silhouette_score(X, model.labels_)
+        score = metrics.silhouette_score(
+            X,
+            model.labels_
+        )
         silhouette_values.append(score)
 
     return ks, inertia_values, silhouette_values
