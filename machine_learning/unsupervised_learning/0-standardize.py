9574a58e7d3b83d922de1af6fa89c94042ff21e0
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-06-13 22:39:02 +0400

    Implement feature standardization

--- machine_learning/unsupervised_learning/0-standardize.py
+++ machine_learning/unsupervised_learning/0-standardize.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-"""Standardization module."""
+"""Feature standardization module."""
 
-from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 
 
 def Standardize(X):
-    """Standardize data."""
-    scaler = StandardScaler()
+    """Standardize tabular data using Scikit-learn.
+
+    Args:
+        X (numpy.ndarray): Input data of shape (n_samples, n_features).
+
+    Returns:
+        numpy.ndarray: Standardized version of X.
+    """
+    scaler = preprocessing.StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
     return X_scaled
